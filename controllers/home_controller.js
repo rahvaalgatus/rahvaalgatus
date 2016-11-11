6921367e2ffa91f31715dcd94d731935c9511d95
Andri Möll <andri@dot.ee>
2016-11-11 00:30:26 +0000

    Refactor HomeController and parallelize initiative fetching.

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,35 +1,29 @@
 var _ = require("lodash")
-var O = require("oolong")
 var Router = require("express").Router
 var api = require("root/lib/citizen_os")
 var readInitiative = api.readInitiative
 var next = require("co-next")
+var ARR = Array.prototype
 
 exports.router = Router({mergeParams: true})
 
 exports.router.get("/", next(function*(req, res, next) {
-	var initiatives = yield {
-		discussions: readInitiatives("inProgress"),
-		votings: readInitiatives("voting"),
-		processes: readInitiatives("followUp"),
+	var initiatives = (yield api(`/api/topics`)).body.data.rows
+	initiatives = _.groupBy(initiatives, "status")
+
+	initiatives = yield {
+		discussions: initiatives.inProgress || ARR,
+		votings: _.map(initiatives.voting || ARR, "id").map(readInitiative),
+		processes: _.map(initiatives.followUp || ARR, "id").map(readInitiative)
 	}
-		
-	initiatives = O.map(initiatives, (res) => res.body.data.rows)
-	var discussions = initiatives.discussions
-	var votings = yield _.map(initiatives.votings, "id").map(readInitiative)
-	var processes = yield _.map(initiatives.processes, "id").map(readInitiative)
 
 	res.render("home/index", {
 		page: "home",
-		discussions: discussions,
-		votings: votings,
-		processes: processes
+		discussions: initiatives.discussions,
+		votings: initiatives.votings,
+		processes: initiatives.processes
 	})
 }))
 
 exports.router.get("/about", (req, res) => res.render("home/about"))
 exports.router.get("/support", (req, res) => res.render("home/support"))
-	
-function readInitiatives(status) {
-	return api(`/api/topics?statuses=${status}`)
-}
