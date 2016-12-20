8fdb7a4d08e06eb89ed468f3fa416a418374308e
Andri Möll <andri@dot.ee>
2016-12-20 17:13:28 +0000

    Request initiatives in different statuses separately.

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,24 +1,15 @@
-var _ = require("lodash")
 var Router = require("express").Router
 var Config = require("root/config")
 var api = require("root/lib/citizen_os")
 var next = require("co-next")
-var ARR = Array.prototype
 
 exports.router = Router({mergeParams: true})
 
 exports.router.get("/", next(function*(req, res, next) {
-	var path = "/api/topics"
-	path += `?sourcePartnerId=${Config.apiPartnerId}`
-	path += "&include[]=vote"
-	path += "&limit=100"
-	var initiatives = (yield api(path)).body.data.rows
-	initiatives = _.groupBy(initiatives, "status")
-
-	initiatives = yield {
-		discussions: initiatives.inProgress || ARR,
-		votings: initiatives.voting || ARR,
-		processes: initiatives.followUp || ARR,
+	var initiatives = yield {
+		discussions: read("inProgress"),
+		votings: read("voting"),
+		processes: read("followUp")
 	}
 
 	res.render("home/index", {
@@ -31,3 +22,14 @@ exports.router.get("/", next(function*(req, res, next) {
 
 exports.router.get("/about", (req, res) => res.render("home/about"))
 exports.router.get("/donate", (req, res) => res.render("home/donate"))
+
+// If not requesting per-status, limit applies to the entire returned set.
+// Saving us from pagination for now.
+function read(status) {
+	var path = "/api/topics"
+	path += `?sourcePartnerId=${Config.apiPartnerId}`
+	path += "&include[]=vote"
+	path += "&limit=100"
+	path += `&statuses=${status}`
+	return api(path).then((res) => res.body.data.rows)
+}
