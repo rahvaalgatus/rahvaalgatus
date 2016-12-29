2a63087d1eafd52c94f8f14f90704118c7c3a09a
Andri Möll <andri@dot.ee>
2016-12-29 11:47:06 +0000

    Refactor.

--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
 var _ = require("lodash")
 var Mocha = require("mocha")
+var slice = Function.call.bind(Array.prototype.slice)
 
 Mocha.prototype.loadFiles = _.wrap(Mocha.prototype.loadFiles, function(orig) {
-	orig.apply(this, Array.prototype.slice.call(arguments, 1))
+	orig.apply(this, slice(arguments, 1))
 
 	// Mocha will not clear files in the bin directory.
 	after(function() { delete require.cache[require.resolve("root/bin/web")] })
