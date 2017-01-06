b834fbd0fc09f8686244182324bc7b757004a08e
Andri Möll <andri@dot.ee>
2017-01-06 23:33:57 +0000

    Hide failed and ended initiatives from home page.

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,20 @@
+var _ = require("lodash")
 var Router = require("express").Router
+var Initiative = require("root/lib/initiative")
 var api = require("root/lib/citizen_os")
 var next = require("co-next")
 
 exports.router = Router({mergeParams: true})
 
 exports.router.get("/", next(function*(req, res, next) {
-	res.render("home/index", yield api.readInitiatives())
+	var initiatives = yield api.readInitiatives()
+
+	var hasEnded = Initiative.hasDiscussionEnded.bind(null, new Date)
+	initiatives.discussions = _.reject(initiatives.discussions, hasEnded)
+	var hasFailed = Initiative.hasVoteFailed.bind(null, new Date)
+	initiatives.votings = _.reject(initiatives.votings, hasFailed)
+
+	res.render("home/index", initiatives)
 }))
 
 exports.router.get("/about", (req, res) => res.render("home/about"))
