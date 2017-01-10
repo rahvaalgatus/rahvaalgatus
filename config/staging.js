b67372967516d3c0d86420010a46fc6d5ee0868b
Andri Möll <andri@dot.ee>
2017-01-10 01:48:43 +0000

    Remove Etherpad theme as it's no longer used.

--- config/staging.js
+++ config/staging.js
@@ -1,5 +1,4 @@
 module.exports = {
 	__proto__: require("./development"),
-	"apiPartnerId": "3bc2e428-8875-4f19-8007-6484ba0d8c58",
-	"etherpadTheme": "raa_staging"
+	"apiPartnerId": "3bc2e428-8875-4f19-8007-6484ba0d8c58"
 }
