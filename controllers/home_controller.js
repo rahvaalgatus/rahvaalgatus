4b19fc2c06e6f75f4f156688911ad3eb741352a5
Andri Möll <andri@dot.ee>
2017-05-10 14:16:00 +0000

    Add govermental ideas page.

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -22,3 +22,7 @@ exports.router.get("/", next(function*(_req, res) {
 exports.router.get("/about", (_req, res) => res.render("home/about"))
 exports.router.get("/donate", (_req, res) => res.render("home/donate"))
 exports.router.get("/effective-ideas", (_req, res) => res.render("home/ideas"))
+
+exports.router.get("/effective-ideas/govermental", function(_req, res) {
+	res.render("home/ideas-for-gov")
+})
