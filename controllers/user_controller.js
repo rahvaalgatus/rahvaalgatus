eb4f6aac896752c57d58385e19ef2aea989fe869
Andri Möll <andri@dot.ee>
2017-05-15 11:53:13 +0000

    Add 401 Unauthorized page.

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,6 @@
 var O = require("oolong")
 var Router = require("express").Router
+var HttpError = require("standard-http-error")
 var isOk = require("root/lib/http").isOk
 var catch400 = require("root/lib/fetch").catch.bind(null, 400)
 var translateCitizenError = require("root/lib/citizen_os").translateError
@@ -7,20 +8,7 @@ var next = require("co-next")
 
 exports.router = Router({mergeParams: true})
 
-exports.read = next(function*(req, res) {
-	var path = "/api/users/self/topics"
-	path += "?include[]=vote"
-	var initiatives = req.api(path)
-	initiatives = yield initiatives.then(getRows)
-
-	res.render("user/read", {
-		user: req.user,
-		initiatives: initiatives,
-		attrs: O.create(req.user, res.locals.attrs)
-	})
-})
-
-exports.router.get("/", exports.read)
+exports.router.get("/", next(read))
 
 exports.router.put("/", next(function*(req, res, next) {
 	var updated = yield req.api("/api/users/self", {
@@ -36,8 +24,21 @@ exports.router.put("/", next(function*(req, res, next) {
 		res.status(422)
 		res.locals.error = translateCitizenError(req.t, updated.body)
 		res.locals.attrs = req.body
-		exports.read(req, res, next)
+		yield read(req, res, next)
 	}
 }))
+
+function* read(req, res) {
+	if (req.user == null) throw new HttpError(401)
+
+	var initiatives = req.api("/api/users/self/topics?include[]=vote")
+	initiatives = yield initiatives.then(getRows)
+
+	res.render("user/read", {
+		user: req.user,
+		initiatives: initiatives,
+		attrs: O.create(req.user, res.locals.attrs)
+	})
+}
 
 function getRows(res) { return res.body.data.rows }
