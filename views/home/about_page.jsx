53aad65e61fcd84337ed38980259b6cd3b0c7cc4
Andri Möll <andri@dot.ee>
2019-03-15 17:07:55 +0000

    Fix "about" page styles.

--- views/home/about_page.jsx
+++ views/home/about_page.jsx
@@ -6,7 +6,7 @@ module.exports = function(attrs) {
 	var req = attrs.req
 	var t = req.t
 
-	return <Page id="about" title={t("ABOUT_TITLE")} req={req}>
+	return <Page page="about" title={t("ABOUT_TITLE")} req={req}>
 		<section id="about" class="primary-section text-section"><center>
 			{Jsx.html(t("ABOUT_CONTENT"))}
 		</center></section>
