860f12ca2e6bb442e3c71cfaaa35f8cdacc41d55
Andri Möll <andri@dot.ee>
2019-04-26 20:14:02 +0000

    Inline function.

--- test/email.js
+++ test/email.js
@@ -1,7 +1,5 @@
 var sendEmail = require("root").sendEmail
 
 module.exports = function() {
-	beforeEach(clear)
+	beforeEach(function() { this.emails = sendEmail.sent = [] })
 }
-
-function clear() { this.emails = sendEmail.sent = [] }
