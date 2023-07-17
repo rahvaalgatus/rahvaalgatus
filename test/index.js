856a326f3423235593df436ba27f92590fc94aed
Andri Möll <andri@dot.ee>
2023-07-17 08:41:21 +0000

    Remove JSX file uncaching workaround for Mocha.

--- test/index.js
+++ test/index.js
@@ -15,8 +15,4 @@ Mocha.prototype.loadFiles = _.wrap(Mocha.prototype.loadFiles, function(orig) {
 	// Mocha will not clear files in the bin directory.
 	after(function() { delete CACHE[require.resolve("root/bin/adm")] })
 	after(function() { delete CACHE[require.resolve("root/bin/web")] })
-
-	after(function() {
-		for (var path in CACHE) if (path.endsWith(".jsx")) delete CACHE[path]
-	})
 })
