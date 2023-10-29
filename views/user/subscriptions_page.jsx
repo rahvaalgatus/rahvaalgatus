ddaa14f9f9122503b306c2793ab9ec9329d18830
Andri Möll <andri@dot.ee>
2023-10-29 12:29:39 +0000

    Refactor user subscriptions page.

--- views/user/subscriptions_page.jsx
+++ views/user/subscriptions_page.jsx
@@ -14,21 +14,22 @@ module.exports = function(attrs) {
 
 	return <UserPage
 		page="user-subscriptions"
+		title={t("user_subscriptions_page.title")}
 		req={req}
 		user={user}
 	>
 		<Section id="profile" class="primary-section">
 			{user.email == null ? <p class="unconfirmed-email-info">
-				{Jsx.html(t("USER_SUBSCRIPTIONS_UNCONFIRMED_EMAIL", {
+				{Jsx.html(t("user_subscriptions_page.subscriptions.unconfirmed_email", {
 					emailUrl: _.escapeHtml(req.baseUrl)
 				}))}
 			</p> : subscriptions.length == 0 ? <p class="no-subscriptions">
-				{t("USER_SUBSCRIPTIONS_EMPTY")}
+				{t("user_subscriptions_page.subscriptions.empty")}
 			</p> : <>
 				<Flash flash={req.flash} />
 
 				<p class="text">
-					{Jsx.html(t("SUBSCRIPTIONS_UPDATE_BODY", {
+					{Jsx.html(t("user_subscriptions_page.subscriptions.description", {
 						email: _.escapeHtml(user.email)
 					}))}
 				</p>
